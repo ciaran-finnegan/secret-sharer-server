1a471966caf8a5c557add5f5017d6df29b5c4af8
Ryan Glover <business@cleverbeagle.com>
2021-01-29 17:59:32 -0600

    get stripe billing portal session working wire up billing portal link on settings page

--- create-billing-portal-session.js
+++ create-billing-portal-session.js
@@ -1,4 +1,4 @@
-// import stripePackage from "stripe";
+import stripePackage from "stripe";
 import handler from "./libs/handler-lib";
 // import { getPriceId } from "./libs/billing-lib";
 // import getItem from "./libs/dynamodb/getItem";
@@ -7,84 +7,42 @@ import AWS from "aws-sdk";
 import getCognitoUser from "./libs/getCongitoUser";
 
 export const main = handler(async (event, context) => {
-  //console.log(process.env);
-  const { email } = JSON.parse(event.body);
-  console.log(`email sent from client: ${email}`);
-  // const domainURL = redirectURL || process.env.domainURL;
-  // const priceId = getPriceId(subscriptionName);
-  // const stripe = stripePackage(process.env.stripeSecretKey);
-
-  const authProvider =
-    event.requestContext.identity.cognitoAuthenticationProvider;
-  // Cognito authentication provider looks like:
-  // cognito-idp.us-east-1.amazonaws.com/us-east-1_xxxxxxxxx,cognito-idp.us-east-1.amazonaws.com/us-east-1_aaaaaaaaa:CognitoSignIn:qqqqqqqq-1111-2222-3333-rrrrrrrrrrrr
-  // Where us-east-1_aaaaaaaaa is the User Pool id
-  // And qqqqqqqq-1111-2222-3333-rrrrrrrrrrrr is the User Pool User Id
-  const parts = authProvider.split(":");
-  const userPoolIdParts = parts[parts.length - 3].split("/");
-
-  const userPoolId = userPoolIdParts[userPoolIdParts.length - 1];
-  const userPoolUsername = parts[parts.length - 1];
-
-  console.log(`userPoolId, ${userPoolId}`);
-  console.log(
-    `Cognito userPoolUsername for logged in user on client: ${userPoolUsername}`
-  );
-
-  console.log('DEBUG:: Calling getCognitoUser(userPoolUsername)');
-  let cognitoUser = await getCognitoUser(userPoolUsername);
-  //
-  //
-  //
-  // Ask Ryan how to make this fucking thing wait!!!
-  //
-  //
-  //
-  console.log('DEBUG:: getCognitoUser(userPoolUsername) returned the user email: ');
-  console.log(cognitoUser.UserAttributes[2].Value);
-  let cognitoUserEmail = cognitoUser.UserAttributes[2].Value;
-
-  const documentClient = new AWS.DynamoDB.DocumentClient();
-  const user = await documentClient
-    .scan({
-      TableName: "dev-users",
-      ScanIndexForward: true,
-      FilterExpression: "#DYNOBASE_email = :email",
-      ExpressionAttributeNames: {
-        "#DYNOBASE_email": "email",
-      },
-      ExpressionAttributeValues: {
-        ":email": cognitoUserEmail,
-      },
-    })
-    .promise();
-//
-//
-// Qury should work, object not getting created in DynamoDB on signup (was working before?)
-//
-//
-
-  // console.log(`user: ${JSON.stringify(user, null, 2)}`);
-
-  console.log(JSON.stringify(user, null, 2));
-
   try {
-    // DONE: Strip the Cognito User ID from the event sent from the client to the serverless function
-    // DONE: Query Cognito and retrieve the email for that user
-    // (note - safer than accepting an email from the client which could be tampered with, I think - need to think about this)
-    // DONE: In here, based on email, pull the customer record from dynamo and get stripe customer ID.
-    // NOTE: https://stripe.com/docs/billing/subscriptions/integrating-customer-portal#redirect
-    // const session = await stripe.billingPortal.sessions.create({
-    //   customer_email: email,
-    //   return_url: `${domainURL}/settings`,
-    // });
-    // return {
-    //   turkey: true,
-    //   session: { id: 123 },
-    //   // status: true,
-    //   // sessionId: session.id,
-    // };
-    // TODO - Call the Stripe billing portal with their customer ID
+    const domainURL = process.env.domainURL;
+    const stripe = stripePackage(process.env.stripeSecretKey);
+
+    // NOTE: See notes file at root of project for details on how this works.
+    const authProvider =
+      event.requestContext.identity.cognitoAuthenticationProvider;
+    const parts = authProvider.split(":");
+    const userPoolUsername = parts[parts.length - 1];
+    const cognitoUser = await getCognitoUser(userPoolUsername);
+    const cognitoUserEmail = cognitoUser.UserAttributes[2].Value;
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+
+    const { Items } = await documentClient
+      .scan({
+        TableName: "dev-users",
+        ScanIndexForward: true,
+        FilterExpression: "#DYNOBASE_email = :email",
+        ExpressionAttributeNames: {
+          "#DYNOBASE_email": "email",
+        },
+        ExpressionAttributeValues: {
+          ":email": cognitoUserEmail,
+        },
+      })
+      .promise();
+
+    const user = Items && Items.find((item) => item.email === cognitoUserEmail);
+    const session = await stripe.billingPortal.sessions.create({
+      customer: user && user.customerId,
+      return_url: `${domainURL}/settings`,
+    });
+
+    return {
+      session,
+    };
   } catch (e) {
     return {
       status: false,
--- libs/getCongitoUser.js
+++ libs/getCongitoUser.js
@@ -1,75 +1,33 @@
 // import AWS from "aws-sdk";
 // import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
 //ciaran - testing
-const AWS = require('aws-sdk');
-const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({apiVersion: '2016-04-18'});
-
+const AWS = require("aws-sdk");
+const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({
+  apiVersion: "2016-04-18",
+});
 
 export default async (userId) => {
   // console.log(process.env);
   var params = {
     UserPoolId: process.env.COGNITO_USER_POOL_ID,
-    Username: userId
+    Username: userId,
   };
-  console.log('DEBUG:: Cognito params set to');
+  console.log("DEBUG:: Cognito params set to");
   console.log(params);
 
   // Retrieve User Object from Cognito User Pool
 
-  let user = await cognitoidentityserviceprovider.adminGetUser(params).promise();
+  let user = await cognitoidentityserviceprovider
+    .adminGetUser(params)
+    .promise();
 
-   //
-   //
-   // Ask Ryan how to do error handling for this
-   //
-   //
+  //
+  //
+  // Ask Ryan how to do error handling for this
+  //
+  //
 
-  console.log('DEBUG:: User\'s email is: ');
+  console.log("DEBUG:: User's email is: ");
   console.log(user.UserAttributes[2].Value);
   return user;
-
-  // cognitoidentityserviceprovider.adminGetUser(params, function(err, data) {
-  //   if (err) {
-  //     console.log('DEBUG:: Error calling cognitoidentityserviceprovider.adminGetUser');
-  //     console.log(err, err.stack);
-  //     return(err);
-  //   }
-  //   else     {
-  //     console.log('DEBUG:: Success, cognitoidentityserviceprovider.adminGetUser returned: ');
-  //     console.log(data);
-  //     console.log('DEBUG:: User\'s Attributes are: ');
-  //     console.log(data.UserAttributes);
-  //     console.log('DEBUG:: User\'s email is: ');
-
-  //     console.log(data.UserAttributes[2].Value);
-  //     //
-  //     //
-  //     //
-  //     // Ask Ryan about the best way to do this assuming the email may not always be the 3rd item in the array
-  //     //
-  //     //
-
-  //     return(data);
-  //   }
-  // });
 };
-
-// AWS.config.region = process.env.COGNITO_REGION;
-  // AWS.config.credentials = new AWS.CognitoIdentityCredentials(
-  //   {
-  //     IdentityPoolId: process.env.COGNITO_IDENTITY_POOL_ID,
-  //   },
-  //   {
-  //     region: "ap-southeast-2",
-  //   }
-  // );
-
-  // const Pool = new AmazonCognitoIdentity.CognitoUserPool({
-  //   UserPoolId: process.env.COGNITO_USER_POOL_ID,
-  //   ClientId: process.env.COGNITO_APP_CLIENT_ID,
-  // });
-
-  // const cognitoUser = new AmazonCognitoIdentity.CognitoUser({
-  //   Username,
-  //   Pool,
-  // });
