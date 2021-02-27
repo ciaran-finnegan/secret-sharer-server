a8fae802400fe11c644e2aa2c337044e3a638a14
Ryan Glover <business@cleverbeagle.com>
2021-02-26 19:07:27 -0600

    wip wiring up new business model

--- libs/billing-lib.js
+++ libs/billing-lib.js
@@ -1,43 +1,66 @@
 export function getPriceId(subscriptionName) {
-
   // Allows changes to Stripe Products without changing client
   // Move these to Env Variables or a table
-  const businessSubscriptionPriceId  = process.env.businessSubscriptionPriceId;
-  const enterpriseSubscriptionPriceId = process.env.enterpriseSubscriptionPriceId;
+  // const enterpriseSubscriptionPriceId =
+  // process.env.enterpriseSubscriptionPriceId;
 
-  if (subscriptionName === "Business") {
-    return businessSubscriptionPriceId;
+  const soloSubscriptionPriceId = process.env.soloSubscriptionPriceId;
+  const proSubscriptionPriceId = process.env.proSubscriptionPriceId;
+
+  if (subscriptionName === "free") {
+    return null;
   }
-  if (subscriptionName === "Enterprise") {
-    return enterpriseSubscriptionPriceId;
+
+  if (subscriptionName === "solo") {
+    return soloSubscriptionPriceId;
   }
+
+  if (subscriptionName === "pro") {
+    return proSubscriptionPriceId;
+  }
+
+  // if (subscriptionName === "Enterprise") {
+  //   return enterpriseSubscriptionPriceId;
+  // }
 
   // Add Error Handling  here
 }
 
 export function calculateCost(subscriptionId) {
-
   // Move these to Env Variables
-  const businessSubscriptionMonthlyRate  = 49.00;
-  const businessSubscriptionDescription  = "Business";
+  const basicSubscriptionMonthlyRate = 4.99;
+  const basicSubscriptionDescription = "Basic";
+  const businessSubscriptionMonthlyRate = 49.0;
+  const businessSubscriptionDescription = "Business";
   const enterpriseSubscriptionMonthlyRate = 149.99;
   const enterpriseSubscriptionDescription = "Enterprise";
 
   if (subscriptionId === "1") {
     let sub = {
-      "amount" : businessSubscriptionMonthlyRate,
-      "description" : businessSubscriptionDescription
+      amount: basicSubscriptionMonthlyRate,
+      description: basicSubscriptionDescription,
     };
+
     return sub;
   }
+
   if (subscriptionId === "2") {
     let sub = {
-      "amount" : enterpriseSubscriptionMonthlyRate,
-      "description" : enterpriseSubscriptionDescription
+      amount: businessSubscriptionMonthlyRate,
+      description: businessSubscriptionDescription,
     };
-    return sub;
 
+    return sub;
   }
+
+  if (subscriptionId === "3") {
+    let sub = {
+      amount: enterpriseSubscriptionMonthlyRate,
+      description: enterpriseSubscriptionDescription,
+    };
+
+    return sub;
+  }
+
   // Add Error Handling  here
 }
-
