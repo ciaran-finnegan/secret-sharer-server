5c7ca3cb4094d479674f2112b9d80a55a4a45c46
slartibastfast <ciaran.finnegan@gmail.com>
2021-03-08 12:20:35 +0800

    added get-subscription-status

---
+++ get-subscription-status.js
@@ -0,0 +1,49 @@
+// import AWS from "aws-sdk";
+import handler from "./libs/handler-lib";
+import getCognitoUser from "./libs/getCongitoUser";
+import AWS from "aws-sdk";
+
+export const main = handler(async (event, context) => {
+  // NOTE: See notes file at root of project for details on how this works.
+  console.log(JSON.stringify(event, null, 2));
+  const authProvider =
+    event.requestContext.identity.cognitoAuthenticationProvider;
+  const parts = authProvider.split(":");
+  const userPoolUsername = parts[parts.length - 1];
+  const cognitoUser = await getCognitoUser(userPoolUsername);
+
+  console.log(`DEBUG: CognitoUsername: ${cognitoUser.Username}`);
+
+  const date = new Date();
+  const firstDayOfMonth= new Date(date.getFullYear(), date.getMonth(), 1);
+
+  console.log(`DEBUG: firstDayOfMonth: ${firstDayOfMonth.getTime()} `);
+
+
+  const documentClient = new AWS.DynamoDB.DocumentClient();
+
+  const { Count } = await documentClient
+  .query({
+    "TableName": process.env.tableName,
+    "IndexName": "createdBy-index",
+    "KeyConditionExpression": "#DYNOBASE_createdBy = :pkey",
+    "ExpressionAttributeValues": {
+      ":pkey": cognitoUser.Username,
+      ":createdAt": firstDayOfMonth.getTime()
+    },
+    "ExpressionAttributeNames": {
+      "#DYNOBASE_createdBy": "createdBy",
+      "#DYNOBASE_createdAt": "createdAt"
+    },
+    "ScanIndexForward": true,
+    "Limit": 100,
+    "FilterExpression": "#DYNOBASE_createdAt >= :createdAt"
+  })
+  .promise();
+
+  console.log(`DEBUG: Count: ${Count} `);
+
+  return {
+    seretsCreatedThisMonth: Count,
+  };
+});
