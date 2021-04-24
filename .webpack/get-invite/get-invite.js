35c22aaa0cf750b6bf205aff3801d3dcdff53c79
rglover <me@ryanglover.net>
2021-04-23 19:26:15 -0500

    wip wiring up invite acceptance

--- .webpack/get-invite/get-invite.js
+++ .webpack/get-invite/get-invite.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 20);
+/******/ 	return __webpack_require__(__webpack_require__.s = 19);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -94,7 +94,7 @@ module.exports = require("aws-sdk");
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(8).install();
+__webpack_require__(7).install();
 
 
 /***/ }),
@@ -605,7 +605,7 @@ exports.computeSourceURL = computeSourceURL;
 var base64VLQ = __webpack_require__(4);
 var util = __webpack_require__(2);
 var ArraySet = __webpack_require__(5).ArraySet;
-var MappingList = __webpack_require__(12).MappingList;
+var MappingList = __webpack_require__(11).MappingList;
 
 /**
  * An instance of the SourceMapGenerator represents a source map which is
@@ -1063,7 +1063,7 @@ exports.SourceMapGenerator = SourceMapGenerator;
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-var base64 = __webpack_require__(11);
+var base64 = __webpack_require__(10);
 
 // A single base 64 digit can contain 6 bits of data. For the base 64 variable
 // length quantities we use in the source map spec, the first bit is the sign,
@@ -1303,20 +1303,14 @@ module.exports = require("util");
 
 /***/ }),
 /* 7 */
-/***/ (function(module, exports) {
-
-module.exports = require("crypto");
-
-/***/ }),
-/* 8 */
 /***/ (function(module, exports, __webpack_require__) {
 
-/* WEBPACK VAR INJECTION */(function(module) {var SourceMapConsumer = __webpack_require__(10).SourceMapConsumer;
-var path = __webpack_require__(17);
+/* WEBPACK VAR INJECTION */(function(module) {var SourceMapConsumer = __webpack_require__(9).SourceMapConsumer;
+var path = __webpack_require__(16);
 
 var fs;
 try {
-  fs = __webpack_require__(18);
+  fs = __webpack_require__(17);
   if (!fs.existsSync || !fs.readFileSync) {
     // fs doesn't have all methods we need
     fs = null;
@@ -1325,7 +1319,7 @@ try {
   /* nop */
 }
 
-var bufferFrom = __webpack_require__(19);
+var bufferFrom = __webpack_require__(18);
 
 /**
  * Requires a module which is protected against bundler minification.
@@ -1916,10 +1910,10 @@ exports.resetRetrieveHandlers = function() {
   retrieveFile = handlerExec(retrieveFileHandlers);
 }
 
-/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(9)(module)))
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(8)(module)))
 
 /***/ }),
-/* 9 */
+/* 8 */
 /***/ (function(module, exports) {
 
 module.exports = function(module) {
@@ -1947,7 +1941,7 @@ module.exports = function(module) {
 
 
 /***/ }),
-/* 10 */
+/* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 /*
@@ -1956,12 +1950,12 @@ module.exports = function(module) {
  * http://opensource.org/licenses/BSD-3-Clause
  */
 exports.SourceMapGenerator = __webpack_require__(3).SourceMapGenerator;
-exports.SourceMapConsumer = __webpack_require__(13).SourceMapConsumer;
-exports.SourceNode = __webpack_require__(16).SourceNode;
+exports.SourceMapConsumer = __webpack_require__(12).SourceMapConsumer;
+exports.SourceNode = __webpack_require__(15).SourceNode;
 
 
 /***/ }),
-/* 11 */
+/* 10 */
 /***/ (function(module, exports) {
 
 /* -*- Mode: js; js-indent-level: 2; -*- */
@@ -2034,7 +2028,7 @@ exports.decode = function (charCode) {
 
 
 /***/ }),
-/* 12 */
+/* 11 */
 /***/ (function(module, exports, __webpack_require__) {
 
 /* -*- Mode: js; js-indent-level: 2; -*- */
@@ -2119,7 +2113,7 @@ exports.MappingList = MappingList;
 
 
 /***/ }),
-/* 13 */
+/* 12 */
 /***/ (function(module, exports, __webpack_require__) {
 
 /* -*- Mode: js; js-indent-level: 2; -*- */
@@ -2130,10 +2124,10 @@ exports.MappingList = MappingList;
  */
 
 var util = __webpack_require__(2);
-var binarySearch = __webpack_require__(14);
+var binarySearch = __webpack_require__(13);
 var ArraySet = __webpack_require__(5).ArraySet;
 var base64VLQ = __webpack_require__(4);
-var quickSort = __webpack_require__(15).quickSort;
+var quickSort = __webpack_require__(14).quickSort;
 
 function SourceMapConsumer(aSourceMap, aSourceMapURL) {
   var sourceMap = aSourceMap;
@@ -3270,7 +3264,7 @@ exports.IndexedSourceMapConsumer = IndexedSourceMapConsumer;
 
 
 /***/ }),
-/* 14 */
+/* 13 */
 /***/ (function(module, exports) {
 
 /* -*- Mode: js; js-indent-level: 2; -*- */
@@ -3387,7 +3381,7 @@ exports.search = function search(aNeedle, aHaystack, aCompare, aBias) {
 
 
 /***/ }),
-/* 15 */
+/* 14 */
 /***/ (function(module, exports) {
 
 /* -*- Mode: js; js-indent-level: 2; -*- */
@@ -3507,7 +3501,7 @@ exports.quickSort = function (ary, comparator) {
 
 
 /***/ }),
-/* 16 */
+/* 15 */
 /***/ (function(module, exports, __webpack_require__) {
 
 /* -*- Mode: js; js-indent-level: 2; -*- */
@@ -3926,19 +3920,19 @@ exports.SourceNode = SourceNode;
 
 
 /***/ }),
-/* 17 */
+/* 16 */
 /***/ (function(module, exports) {
 
 module.exports = require("path");
 
 /***/ }),
-/* 18 */
+/* 17 */
 /***/ (function(module, exports) {
 
 module.exports = require("fs");
 
 /***/ }),
-/* 19 */
+/* 18 */
 /***/ (function(module, exports) {
 
 var toString = Object.prototype.toString
@@ -4013,7 +4007,7 @@ module.exports = bufferFrom
 
 
 /***/ }),
-/* 20 */
+/* 19 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -4026,62 +4020,6 @@ __webpack_require__.d(__webpack_exports__, "main", function() { return /* bindin
 // EXTERNAL MODULE: /Users/rglover/projects/cleverbeagle/customers/ciaran-finnegan/sss/node_modules/source-map-support/register.js
 var register = __webpack_require__(1);
 
-// EXTERNAL MODULE: external "crypto"
-var external_crypto_ = __webpack_require__(7);
-var external_crypto_default = /*#__PURE__*/__webpack_require__.n(external_crypto_);
-
-// CONCATENATED MODULE: /Users/rglover/projects/cleverbeagle/customers/ciaran-finnegan/sss/node_modules/uuid/dist/esm-node/rng.js
-
-function rng() {
-  return external_crypto_default.a.randomBytes(16);
-}
-// CONCATENATED MODULE: /Users/rglover/projects/cleverbeagle/customers/ciaran-finnegan/sss/node_modules/uuid/dist/esm-node/bytesToUuid.js
-/**
- * Convert array of 16 byte values to UUID string format of the form:
- * XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
- */
-var byteToHex = [];
-
-for (var bytesToUuid_i = 0; bytesToUuid_i < 256; ++bytesToUuid_i) {
-  byteToHex[bytesToUuid_i] = (bytesToUuid_i + 0x100).toString(16).substr(1);
-}
-
-function bytesToUuid(buf, offset) {
-  var i = offset || 0;
-  var bth = byteToHex; // join used to fix memory issue caused by concatenation: https://bugs.chromium.org/p/v8/issues/detail?id=3175#c4
-
-  return [bth[buf[i++]], bth[buf[i++]], bth[buf[i++]], bth[buf[i++]], '-', bth[buf[i++]], bth[buf[i++]], '-', bth[buf[i++]], bth[buf[i++]], '-', bth[buf[i++]], bth[buf[i++]], '-', bth[buf[i++]], bth[buf[i++]], bth[buf[i++]], bth[buf[i++]], bth[buf[i++]], bth[buf[i++]]].join('');
-}
-
-/* harmony default export */ var esm_node_bytesToUuid = (bytesToUuid);
-// CONCATENATED MODULE: /Users/rglover/projects/cleverbeagle/customers/ciaran-finnegan/sss/node_modules/uuid/dist/esm-node/v4.js
-
-
-
-function v4(options, buf, offset) {
-  var i = buf && offset || 0;
-
-  if (typeof options == 'string') {
-    buf = options === 'binary' ? new Array(16) : null;
-    options = null;
-  }
-
-  options = options || {};
-  var rnds = options.random || (options.rng || rng)(); // Per 4.4, set bits for version and `clock_seq_hi_and_reserved`
-
-  rnds[6] = rnds[6] & 0x0f | 0x40;
-  rnds[8] = rnds[8] & 0x3f | 0x80; // Copy bytes to buffer, if provided
-
-  if (buf) {
-    for (var ii = 0; ii < 16; ++ii) {
-      buf[i + ii] = rnds[ii];
-    }
-  }
-
-  return buf || esm_node_bytesToUuid(rnds);
-}
-
-/* harmony default export */ var esm_node_v4 = (v4);
 // EXTERNAL MODULE: external "util"
 var external_util_ = __webpack_require__(6);
 var external_util_default = /*#__PURE__*/__webpack_require__.n(external_util_);
@@ -4178,101 +4116,26 @@ const dynamodb = new external_aws_sdk_default.a.DynamoDB();
     console.warn(error);
   });
 });
-// CONCATENATED MODULE: /Users/rglover/projects/cleverbeagle/customers/ciaran-finnegan/sss/libs/email-lib.js
-
-
-const aws = __webpack_require__(0); // var ses = new aws.SES({ region: "us-west-2" });
-
-
-const ses = new aws.SES();
-async function sendEmail(toAddresses, sourceEmailAddress, subject, body) {
-  if (!toAddresses || !sourceEmailAddress || !subject || !body) {
-    throw new Error("Must pass toAddresses, sourceEmailAddress, subject and body");
-  } // Sample usage
-  // sendEmail([a@b.com], "noreply@foo.com", "Some subject", "Some body");
-
-
-  const params = {
-    Destination: {
-      ToAddresses: toAddresses
-    },
-    Message: {
-      Body: {
-        Text: {
-          Data: body
-        }
-      },
-      Subject: {
-        Data: subject
-      }
-    },
-    Source: sourceEmailAddress
-  };
-  console.log(params);
-  return ses.sendEmail(params).promise().then(response => {
-    console.log(`DEBUG:: Successfully Sent Email: ${response}}`);
-  }).catch(error => {
-    console.error(`ERROR:: Failed to send email: ${error}}`);
-  });
-}
-// CONCATENATED MODULE: /Users/rglover/projects/cleverbeagle/customers/ciaran-finnegan/sss/resend-team-invite.js
-
-// import AWS from "aws-sdk";
-
+// CONCATENATED MODULE: /Users/rglover/projects/cleverbeagle/customers/ciaran-finnegan/sss/get-invite.js
 
 
 
 const main = handler(async (event, context) => {
   try {
-    const id = esm_node_v4();
+    // Request body is passed in as a JSON encoded string in 'event.body'
     const data = JSON.parse(event.body);
-    const {
-      inviteId
-    } = data;
+    const inviteId = data.inviteId;
     const tableName = process.env.invitesTableName;
+    console.log(`DEBUG: Event: ${event}`);
+    console.log(`DEBUG: data: ${data}`);
     console.log(`DEBUG: tableName: ${tableName}`);
-    console.log(`DEBUG: inviteId: ${data.inviteId}`);
-    console.log(`DEBUG: role: ${data.role}`);
-    console.log(`DEBUG: event: ${JSON.stringify(data)}`);
-    /*
-      TODO:
-       1. Create an invite in the invites table and get back an ID.
-      2. Send an email to the emailAddress.
-      3. Return.
-    */
-
     const invite = await getItem(tableName, {
-      id: inviteId
+      inviteId
     });
-
-    if (invite && invite.emailAddress) {
-      console.log(invite);
-      console.log("TODO: SEND EMAIL HERE");
-      const toAddresses = [invite.emailAddress];
-      const sourceEmailAddress = "ciaran@vanish.link";
-      const subject = "You've been invited to activate your Vanish.link account";
-      const body = `Hi,
-      You have been invited to activate your Vanish.link account.
-      Please click on the link below to sign-up (make sure to use the e-mail address this e-mail was sent to).
-
-      https://vanish.link/signup
-
-      Thanks,
-
-      The Vanish Team.`;
-      console.log({
-        toAddresses,
-        sourceEmailAddress,
-        subject,
-        body
-      });
-      const response = await sendEmail(toAddresses, sourceEmailAddress, subject, body);
-      console.log(`DEBUG: Sent email: ${response}`);
-    }
-
+    console.log(invite);
     return {
-      status: 200,
-      id
+      status: invite ? 200 : 404,
+      invite
     };
   } catch (exception) {
     console.warn(exception);
@@ -4281,4 +4144,4 @@ const main = handler(async (event, context) => {
 
 /***/ })
 /******/ ])));
-//# sourceMappingURL=resend-team-invite.js.map
+//# sourceMappingURL=get-invite.js.map
---
+++ get-invite.js
@@ -0,0 +1,26 @@
+import handler from "./libs/handler-lib";
+import getItem from "./libs/dynamodb/getItem";
+
+export const main = handler(async (event, context) => {
+  try {
+    // Request body is passed in as a JSON encoded string in 'event.body'
+    const data = JSON.parse(event.body);
+    const inviteId = data.inviteId;
+    const tableName = process.env.invitesTableName;
+
+    console.log(`DEBUG: Event: ${event}`);
+    console.log(`DEBUG: data: ${data}`);
+    console.log(`DEBUG: tableName: ${tableName}`);
+
+    const invite = await getItem(tableName, { inviteId });
+
+    console.log(invite);
+
+    return {
+      status: invite ? 200 : 404,
+      invite,
+    };
+  } catch (exception) {
+    console.warn(exception);
+  }
+});
